e34bf3845c30d54f6d95eaee7d38f192e8e97336
Coder12a <38924418+Coder12a@users.noreply.github.com>
2018-11-10 13:06:09 -0600

    Use newer settings

--- config.lua
+++ config.lua
@@ -1,14 +1,6 @@
 ec_config = {}
 
-ec_config.set = function(s,dv)
-	if s then
-		return s
-	else
-		return dv
-	end
-end
-
-ec_config.explosion_max = ec_config.set(tonumber(minetest.setting_get("ec_config.ec_config.explosion_max")),11)
-ec_config.volatile_containers = ec_config.set(minetest.setting_getbool("ec_config.volatile_containers"),true)
-ec_config.volatile_protected_containers = ec_config.set(minetest.setting_getbool("ec_config.volatile_protected_containers"),false)
-ec_config.explosive_traps = ec_config.set(minetest.setting_getbool("ec_config.explosive_traps"),true)
+ec_config.explosion_max = tonumber(minetest.settings:get("ec_config.ec_config.explosion_max")) or 11
+ec_config.volatile_containers = minetest.settings:get_bool("ec_config.volatile_containers") or true
+ec_config.volatile_protected_containers = minetest.settings:get_bool("ec_config.volatile_protected_containers") or false
+ec_config.explosive_traps = minetest.settings:get_bool("ec_config.explosive_traps") or true
