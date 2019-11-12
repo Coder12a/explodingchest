b54e57660497326692bb844dbcbc8a32ed87054e
Panquesito7 <halfpacho@gmail.com>
2019-11-11 21:55:33 -0600

    Print when the mod loads

--- init.lua
+++ init.lua
@@ -2,3 +2,7 @@ local modpath = minetest.get_modpath("explodingchest")
 dofile(modpath .. "/config.lua")
 dofile(modpath .. "/trap.lua")
 dofile(modpath .. "/api.lua")
+
+if minetest.settings:get_bool("log_mods") then
+    minetest.log("action", "[ExplodingChest] Loaded.")
+end
